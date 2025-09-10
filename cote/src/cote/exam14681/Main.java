c32127308b77c4144465f006131343c2055bdb9e
kimkijoong7777 <kimkijoong7777@gmail.com>
2025-09-10 22:32:33 +0900

    0910

--- cote/src/cote/exam14681/Main.java
+++ cote/src/cote/exam14681/Main.java
@@ -1,26 +1,23 @@
 package cote.exam14681;
 import java.util.Scanner;
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String input = scanner.nextLine();
-		String[] strs = input.split(" ");
-		
-		int num = scanner.nextInt();
-		int num2 = scanner.nextInt();
-		
-		if(num > 0 && num2 > 0) {
-			System.out.println("1");
-		}
-		else if(num < 0 && num2 > 0) {
-			System.out.println("2");
-		}
-		else if(num < 0 && num2 < 0) {
-			System.out.println("3");
-		}
-		else {
-			System.out.println("4");
-		}
-		scanner.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        if (x > 0 && y > 0)
+            System.out.printf("1");
+
+        else if (x < 0 && y > 0)
+            System.out.printf("2");
+
+        else if (x < 0 && y < 0)
+            System.out.printf("3");
+
+        else {
+            System.out.printf("4");
+        }
+    }
 }
