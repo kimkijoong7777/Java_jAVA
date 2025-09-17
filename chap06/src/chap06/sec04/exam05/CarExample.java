b556ff3b94fac4e542db08f60e6369c8e0b24d52
kimkijoong7777 <kimkijoong7777@gmail.com>
2025-09-17 15:35:23 +0900

    0917
    2025년 9월 17일

---
+++ chap06/src/chap06/sec04/exam05/CarExample.java
@@ -0,0 +1,12 @@
+package chap06.sec04.exam05;
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		myCar.keyTurn0n();
+		myCar.run();
+		int speed = myCar.getSpeed();
+		System.out.println("현재속도: " + speed + "km/h");
+	}
+
+}
