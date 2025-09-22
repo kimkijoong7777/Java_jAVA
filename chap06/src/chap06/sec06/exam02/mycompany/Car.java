345b9f916508e2e71b005c17bfacde8682fc7430
kimkijoong7777 <kimkijoong7777@gmail.com>
2025-09-22 12:26:15 +0900

    0922
    2025년 9월 22일

---
+++ chap06/src/chap06/sec06/exam02/mycompany/Car.java
@@ -0,0 +1,15 @@
+package chap06.sec06.exam02.mycompany;
+
+import chap06.sec06.exam02.hankook.*;
+import chap06.sec06.exam02.kumho.*;
+import chap06.sec06.exam02.hyundai.Engine;
+
+public class Car {
+
+		Engine engine = new Engine();
+		SnowTire tire1 = new SnowTire();
+		BigWidthTire tire2 = new BigWidthTire();
+		chap06.sec06.exam02.hankook.Tire tire3 = new chap06.sec06.exam02.hankook.Tire();
+		chap06.sec06.exam02.kumho.Tire tire4 = new chap06.sec06.exam02.kumho.Tire();
+
+}
---
+++ chap06/src/chap06/sec06/exam03/package2/C.java
@@ -0,0 +1,8 @@
+package chap06.sec06.exam03.package2;
+
+import chap06.sec06.exam03.package1.*;
+
+public class C {
+	A a;  //(x)    A 클래스 접근 불가(컴파일 에러)
+	B b;  //(o) 
+}
